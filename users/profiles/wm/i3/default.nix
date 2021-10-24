846cb60f177770649a8d2f4289cabd874e8f8064
Ray Andrew <raydreww@gmail.com>
2021-10-24 23:05:22 +0700

    add color to i3

--- users/profiles/wm/i3/default.nix
+++ users/profiles/wm/i3/default.nix
@@ -3,6 +3,7 @@
 let
   mod = "Mod4";
   alt = "Mod1";
+  colors = config.colorscheme.colors;
 in
 {
   xsession = {
@@ -30,6 +31,31 @@ in
 
           "${mod}+Shift+v" = "split h";
           "${mod}+v" = "split v";
+        };
+      };
+
+      extraConfig = ''
+        set $base00 #${colors.base00}
+        set $base01 #${colors.base01}
+        set $base02 #${colors.base02}
+        set $base03 #${colors.base03}
+        set $base04 #${colors.base04}
+        set $base05 #${colors.base05}
+        set $base06 #${colors.base06}
+        set $base07 #${colors.base07}
+        set $base08 #${colors.base08}
+        set $base09 #${colors.base09}
+        set $base0A #${colors.base0A}
+        set $base0B #${colors.base0B}
+        set $base0C #${colors.base0C}
+        set $base0D #${colors.base0D}
+        set $base0E #${colors.base0E}
+        set $base0F #${colors.base0F}
+      '';
+
+      barModule = {
+        color = {
+          background = "$base00";
         };
       };
     };
