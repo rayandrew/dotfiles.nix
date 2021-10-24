2de731191d4562ae68ebbe26e3277f03e746e8f9
Ray Andrew <contact@rayandrew.me>
2021-10-24 16:54:24 -0500

    change urxvt

--- users/profiles/terminal/urxvt/default.nix
+++ users/profiles/terminal/urxvt/default.nix
@@ -3,6 +3,13 @@
 {
   programs.urxvt = {
     enable = true;
+    extraConfig = {
+      "perl-ext-common" = "default,tabbedex,clipboard,keyboard-select,url-select";
+      "perl-lib" = "${pkgs.rxvt-unicode}/lib/urxvt/perl";
+      "clipboard.autocopy" = true;
+      "copyCommand" = "${pkgs.xclip}/bin/xclip -i selection clipboard";
+      "pasteCommand" = "${pkgs.xclip}/bin/xclip -o selection clipboard";
+    };
     keybindings = {
       "M-c" = "perl:clipboard:copy";
       "M-v" = "perl:clipboard:paste";
